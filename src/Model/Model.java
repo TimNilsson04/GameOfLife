5d5f5e3e2298ed863da92fb73e28579b4e2c4122
TimNilsson04 <tim.nilsson2@elev.ga.ntig.se>
2023-01-12 13:38:35 +0100

    Har gjort en checkNeighbour som kollar alla pixlar på banan. Denna ska användas för att kolla om döda pixlar ska leva nästa generation. Sedan har jag skapat en getNeigbhour som kollar levande pixlar och denna ska användas för att kolla om den levande pixeln ska leva nästa generation.

--- src/Model/Model.java
+++ src/Model/Model.java
@@ -5,6 +5,7 @@ public class Model {
         int width;
         int height;
 
+
     public Model(int width, int height) {
         this.width = width;
         this.height = height;
@@ -12,8 +13,11 @@ public class Model {
 
     public void update(Point[] points) {
             for (int i = 0; i < points.length; i++){
-                checkNeigbhours(points, width, height);
+              int neigbhour =  getNeighbour(points);
+
             }
+
+
     }
 
     public int checkNeigbhours(Point[] points, int width, int height){
@@ -21,26 +25,54 @@ public class Model {
             int x;
             int y;
 
-            for (int i = 0; i < height; i++) {
-                y = i;
-                x = 1;
-                for (int o = 0; o < width; o++) {
+            for (int i = 0; i<points.length; i++) {
 
-                    if (x - points[i].getX() <= 1 && x - points[i].getX() >= 1 && y - points[i].getY() <= 1 && y - points[i].getY() >= 1) {
-                        neigbhours++;
+                for (int p = 1; p < height; p++) {
+                    y = p;
+                    x = 1;
+
+                    for (int o = 2; o < width; o++) {
+                        if (x - points[i].getX() <= 1 && x - points[i].getX() >= -1 && y - points[i].getY() <= 1 && y - points[i].getY() >= -1) {
+                            neigbhours++;
+                        }
+                        x = o;
                     }
                 }
+
             }
-        return neigbhours;
-    }
+
+            return neigbhours;
+        }
+
+
+        public int getNeighbour(Point[] points){
+        int neigbhour = -1;
+
+        for (int x = 0; x < points.length; x++){
+            for (int i = 0; i < points.length; i++){
+
+                if(points[x].getX() - points[i].getX() <= 1 && points[x].getX() - points[i].getX() >= -1 && points[x].getY() - points[i].getY() <= 1 && points[x].getY() - points[i].getY() >= -1 ) {
+                    neigbhour++;
+                }
+            }
+            System.out.println("pixel " + (x+1) + " has " + neigbhour +" neigbhours");
+
+            if (neigbhour < 2 || neigbhour > 3){
+                // kill it
+            }
+            neigbhour = -1;
+        }
+        return neigbhour;
+        }
 
     public Point[] getPoints() {
-        Point[] points = {new Point(5,5), new Point(5,6), new Point(4,7), new Point(6,7)};
+        Point[] points = {new Point(5,5), new Point(5,6), new Point(6,7)};
         return points;
     }
 
     public Shape[] getShapes() {
-        Point[] points = {new Point(5,5), new Point(5,6), new Point(4,7), new Point(6,7)};
+        Point[] points = {new Point(5,5), new Point(5,6), new Point(6,7)};
         return (Shape[])points;
     }
+
 }
